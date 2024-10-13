059d58225713e2aaf6e36507b3c79489a9b175be
PetrosiunArtem <petrosiun.av@phystech.edu>
2024-10-14 00:43:04 +0300

    Добавлен SortingManagerTest

---
+++ src/test/java/org/example/SortingManagerTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortingManagerTest {
+
+  @Test
+  void sort() {
+    SortingManager manager = new SortingManager();
+    manager.addSortingStrategy(new BubbleSortStrategy());
+    final List<Integer> list = new ArrayList<>();
+    list.add(-3);
+    list.add(2121212);
+    list.add(-5454);
+    assertEquals(manager.sort(list, SortingType.BUBBLE), List.of(-5454, -3, 2121212));
+  }
+}
